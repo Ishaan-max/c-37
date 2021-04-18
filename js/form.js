5e5e5dc8aca11dd892bef8ce3f08c5ceec4df44a
Ishaan-max <77791667+Ishaan-max@users.noreply.github.com>
2021-04-18 16:59:50 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,32 @@
+class Form{
+    constructor(){
+    this.title = createElement('h1')
+    this.input = createInput("Name");
+    this.button = createButton("Play");
+    this.greeting = createElement("h3")
+    }
+    hide(){
+        this.greeting.hide();
+        this.input.hide();
+        this.button.hide();
+    };
+    display(){
+        
+        this.title.html("car racing game");
+        this.title.position(130,0);
+
+        this.input.position(130,160);
+        this.button.position(250,200);
+
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            
+            player.name = this.input.value();
+            playerCount += 1;
+            player.updateCount(playerCount);
+            this.greeting.html("Welcome "+player.name)
+            this.greeting.position(130,160);
+        })
+    }
+}
